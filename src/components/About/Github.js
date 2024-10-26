a7deceda4759302e10f48b4c598fb0094704a188
Iheb Cherif <82654813+Ihebcherif@users.noreply.github.com>
2024-10-26 15:53:07 +0200

    Update Github.js

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -12,7 +12,7 @@ function Github() {
         username="Ihebcherif"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        color="#3be7d7"
         fontSize={16}
       />
     </Row>
